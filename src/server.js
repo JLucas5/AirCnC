affec7e3675d980c88992b323f942e8b64ee8d73
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2019-10-07 14:31:55 -0300

    back-end rest api completed

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express =  require('express')
+const cors = require('cors')
+const mongoose =  require('mongoose')
+const routes =  require('./routes')
+const path = require('path')
+
+const app = express()
+
+mongoose.connect('mongodb+srv://oministack:oministack@oministack-f1kph.mongodb.net/teste?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(cors())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
